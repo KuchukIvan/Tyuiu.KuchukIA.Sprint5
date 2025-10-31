f85dd8ba00bf0eba735566f82c3d4cda480ea6e7
Иван Кучук <Tyuiu.KuchukIA@outlook.com>
2025-10-31 22:40:42 +0500

    Task6

---
+++ Tyuiu.KuchukIA.Sprint5.Task6.V17.Test/DataServiceTest.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using Tyuiu.KuchukIA.Sprint5.Task6.V17.Lib;
+namespace Tyuiu.KuchukIA.Sprint5.Task6.V17.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidLoadFromDataFile()
+        {
+            DataService ds = new DataService();
+            string file = "InPutDataFileTask6V17.txt";
+            string directory = @"C:\DataSprint5";
+            string path = Path.Combine(directory, file);
+
+            int res = ds.LoadFromDataFile(path);
+            int wait = 2;
+
+            Assert.AreEqual(wait, res);
+        }
+
+
+
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string file = "InPutDataFileTask6V17.txt";
+            string directory = @"C:\DataSprint5";
+            string path = Path.Combine(directory, file);
+
+            FileInfo fileInfo = new FileInfo(path);
+
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.KuchukIA.Sprint5.Task6.V17/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Tyuiu.KuchukIA.Sprint5.Task6.V17.Lib;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        Random rnd = new Random();
+        DataService ds = new DataService();
+        Console.Title = "Спринт #4 | Выполнил: И.А. Кучук | ПИНб-25-1";
+        Console.WriteLine("***********************************************************************************************");
+        Console.WriteLine("* Спринт #5                                                                                   *");
+        Console.WriteLine("* Тема: Обработка файлов                                                                      *");
+        Console.WriteLine("* Задание #6                                                                                  *");
+        Console.WriteLine("* Вариант #17                                                                                 *");
+        Console.WriteLine("* Выполнил: Кучук Иван Александрович |  ПИНб-25-1                                             *");
+        Console.WriteLine("***********************************************************************************************");
+        Console.WriteLine("***********************************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                            *");
+        Console.WriteLine("***********************************************************************************************");
+
+
+        
+        string file = "InPutDataFileTask6V17.txt";
+        string directory = @"C:\DataSprint5";
+        string path = Path.Combine(directory, file);
+
+        Console.WriteLine("Данные находятся в файле: " + path);
+
+
+
+
+        Console.WriteLine("***********************************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                  *");
+        Console.WriteLine("***********************************************************************************************");
+
+        int res = ds.LoadFromDataFile(path);
+
+
+        Console.WriteLine(res);
+
+
+        Console.ReadKey();
+
+    }
+}
