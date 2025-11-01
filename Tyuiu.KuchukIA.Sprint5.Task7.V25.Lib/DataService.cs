0444812af80c8fe9e2ffc5ae32c7a25906c5dd8d
Иван Кучук <Tyuiu.KuchukIA@outlook.com>
2025-11-02 00:18:17 +0500

    Task7

---
+++ Tyuiu.KuchukIA.Sprint5.Task7.V25.Lib/DataService.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.KuchukIA.Sprint5.Task7.V25.Lib
+{
+    public class DataService : ISprint5Task7V25
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string file = "OutPutDataFileTask7V25.txt";
+            string directory = @"C:\DataSprint5";
+            string pathSave = Path.Combine(directory, file);
+
+            FileInfo fileInfo = new FileInfo(pathSave);
+            bool fileExists = fileInfo.Exists;
+
+            if (fileExists)
+            {
+                File.Delete(pathSave);
+            }
+
+            string text = File.ReadAllText(path);
+
+            text = Regex.Replace(text, "[a-zA-Z]+", "");
+
+            File.WriteAllText(pathSave, text);
+
+            return pathSave;
+
+
+
+        }
+    }
+}
---
+++ Tyuiu.KuchukIA.Sprint5.Task7.V25.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using Tyuiu.KuchukIA.Sprint5.Task7.V25.Lib;
+namespace Tyuiu.KuchukIA.Sprint5.Task7.V25.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string file = "OutPutDataFileTask7V25.txt";
+            string directory = @"C:\DataSprint5";
+            string path = Path.Combine(directory, file);
+
+            FileInfo fileInfo = new FileInfo(path);
+
+            bool fileExists = fileInfo.Exists;
+
+            bool wait = true;
+
+            Assert.AreEqual(wait, fileExists);
+
+        }
+    }
+}
